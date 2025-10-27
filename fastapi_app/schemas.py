8a62ae60422ac9272c691bdd16045ed4e3d48905
tanaka343 <mnhyodo.yc@kaien-t3.net>
2025-10-27 14:43:06 +0900

    due_dateの型ヒントをdatetimeに変換

--- fastapi_app/schemas.py
+++ fastapi_app/schemas.py
@@ -1,17 +1,18 @@
 from pydantic import BaseModel,Field,ConfigDict
+from datetime import datetime
 
 
 class ItemCreate(BaseModel):
         title     : str = Field(min_length=2,max_length=20,examples=["買い物"])
         content   : str = Field(min_length=2,max_length=20,examples=["アボカド"])
-        due_date  : str = Field(examples=["2025-10-26"])
+        due_date  : datetime = Field(examples=["2025-10-26"])
         completed : bool = Field(examples=[False])
 
 
 class ItemResponse(BaseModel):
         title     : str = Field(min_length=2,max_length=20,examples=["買い物"])
         content   : str = Field(min_length=2,max_length=20,examples=["アボカド"])
-        due_date  : str = Field(examples=["2025-10-26"])
+        due_date  : datetime = Field(examples=["2025-10-26"])
         completed : bool = Field(examples=[False])
 
         model_config = ConfigDict(from_attributes=True)
