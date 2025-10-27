caca6c6e12f3e7745ff92b3e45810bf8099d12c9
tanaka343 <mnhyodo.yc@kaien-t3.net>
2025-10-27 13:38:45 +0900

    データモデル作成

---
+++ fastapi_app/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column,Integer,String,DateTime,Boolean
+from database import Base
+
+class Item(Base):
+  __tablename__ = "tasks"
+  id = Column(Integer,primary_key=True)
+  title = Column(String,nullable=False)   
+  content =Column(String,nullable=True) 
+  due_date  =Column(DateTime,nullable=True)
+  completed = Column(Boolean,default=False)
