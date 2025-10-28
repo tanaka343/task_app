c7563cfdf745e8da365e888c96b2eb64a5266ff8
tanaka343 <mnhyodo.yc@kaien-t3.net>
2025-10-28 15:26:43 +0900

    find_by_due関数を修正し、指定された期間内のアイテムを取得できるようにした

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -4,6 +4,7 @@ from typing import Optional
 from models import Item
 from database import get_db
 from sqlalchemy.orm import Session
+from datetime import date,timedelta
 
 app = FastAPI()
 
@@ -37,11 +38,20 @@ def find_all(db :Session = Depends(get_db)):
 def find_by_id(id :int,db :Session = Depends(get_db)):
     return db.query(Item).filter(Item.id == id).first()
 
-        
-@app.get("/items/",response_model=list[ItemResponse])
-def find_by_due(due_date :str,db :Session=Depends(get_db)):
-    return db.query(Item).filter(Item.due_date == due_date).all()
 
+@app.get("/items/",response_model=list[ItemResponse])
+def find_by_due(due_date :str,end :Optional[int]=None,db :Session=Depends(get_db)):
+    if end is None:
+        return db.query(Item).filter(Item.due_date == due_date).all()
+    else:
+        tasklist=[]
+        from_dt = date.fromisoformat(due_date)
+        # to_dt = from_dt + timedelta(days=end)
+        # return db.query(Item).filter(from_dt <= Item.due_date <=to_dt).all()
+        for i in range(end+1):
+            due = from_dt +timedelta(days=i)
+            tasklist.extend(db.query(Item).filter(Item.due_date==due).all())
+        return tasklist
 
 @app.post("/items",response_model=ItemResponse)
 def create(create_item :ItemCreate,db :Session = Depends(get_db)):
@@ -51,6 +61,18 @@ def create(create_item :ItemCreate,db :Session = Depends(get_db)):
     db.add(new_item)
     db.commit()
     return new_item
+
+
+# @app.get("/items/", response_model=list[ItemResponse])
+# def find_by_due(due_date: str, end: Optional[int] = None, db: Session = Depends(get_db)):
+#     from_dt = date.fromisoformat(due_date)
+
+#     if end is None:
+#         return db.query(Item).filter(Item.due_date == from_dt).all()
+#     else:
+#         to_dt = from_dt + timedelta(days=end)
+#         # ✅ SQLAlchemyの between を使う！
+#         return db.query(Item).filter(Item.due_date.between(from_dt, to_dt)).all()
 
 
 
