b07750fe000e8bad7032026d7b93a102c5a5ddfb
tanaka343 <mnhyodo.yc@kaien-t3.net>
2025-11-04 11:42:42 +0900

    CRUD操作に関するテストケースを追加し、正常系および異常系のテストを実装完了

--- fastapi-app/tests/test_main.py
+++ fastapi-app/tests/test_main.py
@@ -17,17 +17,64 @@ def test_find_by_due_異常系(client_fixture :TestClient):
     assert response.status_code == 404
     assert response.json()["detail"] == "Task not found"
     
-# def test_find_by_due_fromtoday_正常系()
+def test_find_by_due_fromtoday_正常系(client_fixture :TestClient):
+    response = client_fixture.get("/items/today/?end=7")
+    assert response.status_code == 200
+    items = response.json()
+    assert len(items) == 1
 
-# def test_find_by_due_fromtoday_異常系()
+def test_find_by_due_fromtoday_異常系(client_fixture :TestClient):
+    response = client_fixture.get("/items/today/?end=-1")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Task not found"
     
-# def test_find_by_id_正常系()
-# def test_find_by_id_異常系()
+def test_find_by_id_正常系(client_fixture :TestClient):
+    response = client_fixture.get("/items/1")
+    assert response.status_code == 200
+    item = response.json()
+    assert item["id"] == 1
+    
+def test_find_by_id_異常系(client_fixture :TestClient):
+    response = client_fixture.get("/items/10")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Task not found"
 
-# def test_create()
+def test_create(client_fixture :TestClient):
+    response = client_fixture.post("/items",json={"title":"kaimono3","content":"banana","due_date":"2025-11-04","completed":False})
+    assert response.status_code == 201
+    item = response.json()
+    assert item["id"] == 3
+    assert item["title"] == "kaimono3"
+    assert item["content"] == "banana"
+    assert item["due_date"] == "2025-11-04"
+    assert item["completed"] == False
+    response = client_fixture.get("/items")
+    assert len(response.json()) == 3
+
+def test_update_正常系(client_fixture :TestClient):
+    response = client_fixture.put("/items/1",json={"title":"kaimono4","content":"apple","due_date":"2025-11-04","completed":True})
+    assert response.status_code == 200
+    item = response.json()
+    assert item["id"] == 1
+    assert item["title"] == "kaimono4"
+    assert item["content"] == "apple"
+    assert item["due_date"] == "2025-11-04"
+    assert item["completed"] == True
+
+def test_update_異常系(client_fixture :TestClient):
+    response = client_fixture.put("/items/10",json={"title":"kaimono4","content":"apple","due_date":"2025-11-04","completed":True})
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Task not found"
     
-# def test_update_正常系()
-# def test_update_異常系()
-    
-# def test_delete_正常系()
-# def test_delete_異常系()
+def test_delete_正常系(client_fixture :TestClient):
+    response = client_fixture.delete("/items/1")
+    assert response.status_code == 200
+    item = response.json()
+    assert item["id"] == 1
+    response = client_fixture.get("/items")
+    assert len(response.json()) == 1
+
+def test_delete_異常系(client_fixture :TestClient):
+    response = client_fixture.delete("/items/10")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Task not found"
